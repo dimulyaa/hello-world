b11d62f4d4e480a0458cffddc40049b087ed70ea
dimulya <kozlovdimulya98@gmail.com>
2024-09-07 21:38:04 +0300

    new project zadanie 2

---
+++ src/main/kotlin/Main2.kt
@@ -0,0 +1,43 @@
+fun main() {
+
+ var firstName= "Дмитрий"
+//    println(firstName)
+
+    var lastName= "Козлов"
+//println(lastName)
+
+    var height:Double= 1.895451651
+    height=1.7777777
+//    тут я поменял значение переменной задание 8
+//    println("Рост $height")
+
+    var weight:Float =65.3f
+//    println("Вес $weight")
+//    fgfdgdfgff
+
+   var isChild=true
+    isChild=false
+//    тут я поменял значение тру на фолс задание 10 ,не знаю правильно я понял задание
+
+//    var weightRebenka=45
+//    var heightRebenka=1.6f
+//    var facticheskyVes=weightRebenka <40
+//    var facticheskyRost=heightRebenka <1.5f
+//    if (facticheskyRost)
+//        println("this child")
+//    else
+//        println("this is not a child")
+
+    var info="Имя:$firstName,Фамилия:$lastName,Рост:$height,Вес:$weight $isChild"
+    println(info)
+
+
+
+
+
+
+
+
+
+
+}
